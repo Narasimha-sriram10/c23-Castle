0dc41679ce9b32f333b97ada0df1d1bece8ffd47
vishnupriya-whitehatjr <whitehatjr.priya@gmail.com>
2020-03-12 12:27:30 +0530

    canstle head changes

--- sketch.js
+++ sketch.js
@@ -4,29 +4,42 @@ const Bodies = Matter.Bodies;
 
 var engine, world;
 var rect1,rect2,rect3,rect4,rect5,rect6,rect7;
+var triangle,triangle1,triangle2,triangle3,triangle4,triangle5,triangle6,triangle7;
+
 var ground;
 
 function setup(){
-    var canvas = createCanvas(400,400);
+    var canvas = createCanvas(600,400);
     engine = Engine.create();
     world = engine.world;
 
-    rect1 = new Rectangle(70, 200, 50, 200);
-    rect2 = new Rectangle(50, 100, 15, 300);
-    rect3 = new Rectangle(365, 100, 15, 300);
-    rect4 = new Rectangle(300, 200, 50, 200);
-    rect5 = new Rectangle(120, 250, 50, 150);
-    rect6 = new Rectangle(250, 250, 50, 150);
-    rect7 = new Rectangle(170, 280, 80, 120);
-    
-    ground = new Ground(200,390,400,20);
+    rect1 = new Rectangle(90, 300, 50, 200);
+    rect2 = new Rectangle(20, 300, 15, 300);
+    rect3 = new Rectangle(565, 300, 15, 300);
+    rect4 = new Rectangle(500, 300, 50, 200);
+    rect5 = new Rectangle(140, 320, 50, 150);
+    rect6 = new Rectangle(450, 320, 50, 150);
+    rect7 = new Rectangle(385, 320, 80, 120);
+    rect8 = new Rectangle(205, 320, 80, 120);
+    rect9 = new Rectangle(295, 320, 50, 200);
+    triangle1= new Triangle(83,74,3,30);
+    triangle2= new Triangle(118,87,3,30);
+    triangle3= new Triangle(152,77,3,47);
+    triangle4= new Triangle(190,42,3,30);
+    triangle5= new Triangle(242,40,3,47);
+    triangle6= new Triangle(273,22,3,30);
+    triangle7= new Triangle(289,-10,3,30);
+    ground = new Ground(200,390,900,20);
+
     
 }
 
 function draw(){
-    background(0);
+    
+    background("black");
     //text(mouseX + ',' + mouseY, 10, 15);
     Engine.update(engine);
+
     rect1.display();
     rect2.display();
     rect3.display();
@@ -34,10 +47,16 @@ function draw(){
     rect5.display();
     rect6.display();
     rect7.display();
-   
-    fill(150,12,12);
-    triangle(42, 180, 68,118, 88, 180);
-    triangle(147, 259, 188, 132, 228, 258);
-    triangle(289, 180, 315, 118, 337, 180);
+    rect8.display();
+    rect9.display();
+    
+    triangle1.display();
+    triangle2.display();
+    triangle3.display();
+    triangle4.display();
+    triangle5.display();
+    triangle6.display();
+    triangle7.display();
+    
     ground.display();
 }
--- triangle.js
+++ triangle.js
@@ -1,24 +1,24 @@
 class Triangle{
     constructor(x,y,sides,radius){
         var options = {
-            'restitution':0.8,
-            'friction':0.3,
-            'density':1.0
+           
+            isStatic: true,
+            angle: PI/4
         }
         this.body = Bodies.polygon(x, y, sides, radius, options);
-        this.sides = sides;
-        this.radius = radius;
+      
         
         World.add(world, this.body);
     }
     display(){
       var pos =this.body.position;
       var angle = this.body.angle;
+      var vertices= this.body.vertices;
       push();
       translate(pos.x, pos.y);
       rotate(angle);
       fill(150,12,12);
-      regularPolygon(pos.x,pos.y,this.radius,this.sides);
+      triangle(vertices[0].x,vertices[0].y,vertices[1].x,vertices[1].y,vertices[2].x,vertices[2].y);
       pop(); 
     }
     
